fd634952267932b6c4ea0418438b75597202defb
FranAzuara <franazuara@gmail.com>
2024-12-03 09:43:48 +0000

    Final 2.0

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ function sum(num1,num2) {
 // Solo un registro en consola para nosotros
 console.log(sum(7,3))
 
+//Tasas conversion de monedas
+let oneEuroIs = {
+    "JPY": 156.5, // Japan yen
+    "USD": 1.07, // US dollar
+    "GBP": 0.87, // British pound
+};
+
 // Declaramos una función con el nombre exacto "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro) {
     // Convertimos el valor a dólares
@@ -13,22 +20,14 @@ const fromEuroToDollar = function(valueInEuro) {
     // Retornamos el valor en dólares
     return valueInDollar;
 }
-// Declaramos una función con el nombre exacto "formDollarToYen"
-const fromDollarToYen = function(valueInDollar) {
-    // Convertimos el valor a Yen
-    let valueInYen = valueInDollar * 156.5;
-    // Retornamos el valor en Yen
-    return valueInYen;
-}
-// Declaramos una función con el nombre exacto "formYenToPound"
-const fromYenToPound = function(valueInYen) {
-    // Convertimos el valor a Yen
-    let valueInPound = valueInYen * 0.87;
-    // Retornamos el valor en Yen
-    return valueInPound;
+
+function fromDollarToYen(dollars) {
+    return Math.round(((dollars / oneEuroIs["USD"]) * oneEuroIs["JPY"]) * 100) / 100;
 }
 
-
+function fromYenToPound(yen) {
+    return Math.round(((yen / oneEuroIs["JPY"]) * oneEuroIs["GBP"]) *100) / 100;
+}
 // Exporta la función para usarla en otros archivos 
 // (similar a la palabra clave "export" cuando se usa webpack)
 // Tenemos que incluir la función en el exports para que sea exportada a otros archivos como test.js
--- test.js
+++ test.js
@@ -24,30 +24,16 @@ test("One euro should be 1.07 dollars", function() {
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 });
 
-test("One dollar should be 149.61 yen", function() {
+test("One dollar should be 146.26 yen", function() {
     // Importo la funcion desde app.js
     const { fromDollarToYen } = require('./app.js');
-
-    // Uso la función como debe ser usada
-    const yen = fromDollarToYen(2);
-
-    // Si 1 dollar son 156.5 Yens, entonces 2 dolares debe ser (2 * 156.5)
-    const expected = 2 * 156.5;
-
     // Hago mi comparación (la prueba)
-    expect(fromDollarToYen(2)).toBe(313); // 1 dollar son 156.5 Yen, entonces 2 dolares deberían ser = (2 * 156.5)
+    expect(fromDollarToYen(3)).toBe(438.79); // 1 dollar son 146.26 Yen, entonces 3 dolares deberían ser = (3 * 146.26)
 });
 
-test("One Yen should be 0.87 Pounds", function() {
+test("One Yen should be 0.0055 Pounds", function() {
     // Importo la funcion desde app.js
     const { fromYenToPound } = require('./app.js');
-
-    // Uso la función como debe ser usada
-    const pound = fromYenToPound(2);
-
-    // Si 1 Yen son 0.87 Puonds, entonces 2 Yens debe ser (2 * 0.87)
-    const expected = 2 * 0.87;
-
     // Hago mi comparación (la prueba)
-    expect(fromYenToPound(2)).toBe(1.74); // 1 Yen son 0.87 Pounds, entonces 2 Yens deberían ser = (2 * 0.87)
+    expect(fromYenToPound(2000)).toBe(11.12); // 1 Yen son 0.0055 Pounds, entonces 2000 Yens deberían ser = (2000 * 0.0055)
 });
